b6a010a3a5c788d6ed4bcf7042921a4f7d6d1c2e
LuiV228 <luqmaan.toffey@gmail.com>
2024-03-06 13:44:20 +0200

    APICode

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hi");
+import { userRouter, express } from "./controller/UserController.js";
+import { agentsRouter } from "./controller/AgentsController.js";
+import cookieParser from "cookie-parser";
+import { errorHandling } from "./middleware/ErrorHandling.js";
+import path from "path";
+import cors from 'cors';
+const app = express();
+const port = +process.env.PORT || 4251;
+app.use(cors());
+app.use(
+  express.static("./static"),
+  express.json(),
+  express.urlencoded({
+    extended: true,
+  }),
+  cookieParser()
+);
+app.get("^/$|/capstoneProject", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "./static/index.html"));
+});
+app.use("/users", userRouter);
+app.use("/agents", agentsRouter);
+app.use(errorHandling);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
