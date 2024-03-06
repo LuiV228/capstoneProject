cc52c5a76b9793a848664b5e2e8f096020e4a8e9
LuiV228 <luqmaan.toffey@gmail.com>
2024-03-06 13:48:08 +0200

    updated port number

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { errorHandling } from "./middleware/ErrorHandling.js";
 import path from "path";
 import cors from 'cors';
 const app = express();
-const port = +process.env.PORT || 4251;
+const port = +process.env.PORT || 3000;
 app.use(cors());
 app.use(
   express.static("./static"),
