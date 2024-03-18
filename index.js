cc3e60f1aedc28d5585bdb1174a4eebe73dda9f1
LuiV228 <luqmaan.toffey@gmail.com>
2024-03-18 13:22:12 +0200

    problems

--- index.js
+++ index.js
@@ -3,17 +3,26 @@ import { agentsRouter } from "./controller/AgentsController.js";
 import cookieParser from "cookie-parser";
 import { errorHandling } from "./middleware/ErrorHandling.js";
 import path from "path";
-import cors from 'cors';
+import cors from "cors";
 const app = express();
 const port = +process.env.PORT || 3000;
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Request-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Expose-Headers", "Authorization");
+  next();
+});
 app.use(
   express.static("./static"),
   express.json(),
   express.urlencoded({
     extended: true,
   }),
-  cookieParser()
+  cookieParser(),
+  cors()
 );
 app.get("^/$|/capstoneProject", (req, res) => {
   res.status(200).sendFile(path.join(__dirname, "./static/index.html"));
